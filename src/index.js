a835d7d0d5d63520c4af83ba0e006c84276d8900
Paul Davis <pjdavis@gmx.com>
2019-06-03 21:07:59 +0100

    Move createstore out from index

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {applyMiddleware, createStore} from 'redux';
-import rootReducer from "./store/reducers/root";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import thunk from "redux-thunk";
 import Login from './components/auth/login';
+import { store } from "./store/combined";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
 
 ReactDOM.render(
     <Provider store={store}>
